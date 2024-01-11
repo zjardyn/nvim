7d467cc966fa345ac79fbb120c1a2ed417aec71a
zjardyn <zlieraho@uwaterloo.ca>
2024-01-11 02:28:21 -0500

    added settings

--- lua/user/settings.lua
+++ lua/user/settings.lua
@@ -1,3 +1,9 @@
+--    /|    / /                                               //   ) )                    //  ) )          
+--   //|   / /  ___      ___             ( )  _   __         //         ___       __   __//__  ( )  ___    
+--  // |  / / //___) ) //   ) ) ||  / / / / // ) )  ) )     //        //   ) ) //   ) ) //    / / //   ) ) 
+-- //  | / / //       //   / /  || / / / / // / /  / /     //        //   / / //   / / //    / / ((___/ /  
+--//   |/ / ((____   ((___/ /   ||/ / / / // / /  / /     ((____/ / ((___/ / //   / / //    / /   //__     
+
 vim.o.signcolumn = "no"
 vim.o.incsearch = true
 vim.o.number = true
@@ -7,6 +13,7 @@ vim.o.swapfile = false
 vim.o.backup = false
 vim.o.undodir = os.getenv("HOME") .. "/.vim/undodir"
 vim.o.autochdir = true
+vim.o.clipboard = "unnamedplus"
 -- vim.o.undofile = true
 -- vim.o.tabstop = 2
 -- vim.o.shiftwidth = 2
